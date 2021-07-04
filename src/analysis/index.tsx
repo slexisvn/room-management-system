ae275721190da542732f4a8afd8b2aebd86b2cf5
Sinh <slexisvn@gmail.com>
2021-07-04 14:02:41 +0700

    chore: initial

--- src/analysis/index.tsx
+++ src/analysis/index.tsx
@@ -8,21 +8,36 @@ import Line from '@ant-design/charts/es/line';
 
 type ICustomRoom = IRoom & { kindOfRoom: IKindOfRoom };
 
-type ICustomAgreements = (IAgreement & { roomRates: number; room: ICustomRoom })[];
+type ICustomAgreements = (IAgreement & { room: ICustomRoom })[];
 
 const Analysis: FC = () => {
   const db = window.roomManagementSystemDB;
   const [roomData, setRoomData] = useState<IRoom[]>([]);
   const [agreementData, setAgreementData] = useState<ICustomAgreements>([]);
-  const [monthValue, setMonthValue] = useState<moment.Moment[]>([]);
+  const [monthValue, setMonthValue] = useState<number[]>([]);
   const stillEmptyNumber = roomData.filter(room => !room.stillEmpty).length;
 
   const fetchRoomData = async () => {
     setRoomData(await db!.room!.toArray());
   };
 
+  const fetchAgreementData = async () => {
+    const agreements = (await db!.agreement!.toArray()) as ICustomAgreements;
+    await Promise.all(
+      agreements.map(async agreement => {
+        [agreement.room] = (await Promise.all([db!.room!.get(agreement.roomId)])) as ICustomRoom[];
+
+        [agreement.room!.kindOfRoom] = (await Promise.all([
+          db!.kindOfRoom!.get(agreement.room.kindOfRoomId)
+        ])) as IKindOfRoom[];
+      })
+    );
+    setAgreementData(agreements);
+  };
+
   useEffect(() => {
     fetchRoomData();
+    fetchAgreementData();
     // eslint-disable-next-line
   }, []);
 
@@ -57,17 +72,27 @@ const Analysis: FC = () => {
     interactions: [{ type: 'element-active' }]
   };
 
-  const startMonth = monthValue.length > 0 ? moment(monthValue[0]) : moment();
+  const filteredAgreementData = agreementData.filter(
+    agreement =>
+      moment(monthValue[0]).isSameOrBefore(agreement.date[0]) ||
+      moment(monthValue[1]).isSameOrAfter(agreement.date[0])
+  );
 
   const lineData =
     monthValue.length > 0
       ? Array.from({
-          length: moment(monthValue[1]).diff(startMonth, 'months')
-        }).map(number => ({
-          month: startMonth.add((number as number) - 1, 'months').format('MM/YYYY')
-          // value: agreementData.reduce((a, b) => a.)
+          length: moment(monthValue[1]).diff(moment(monthValue[0]), 'months') + 1
+        }).map((_, index) => ({
+          month: moment(monthValue[0]).add(index, 'month').format('MM/YYYY'),
+          value: filteredAgreementData
+            .filter(agreement =>
+              moment(monthValue[0]).add(index, 'month').isSameOrBefore(agreement.date[1])
+            )
+            .map(agreement => agreement.room.kindOfRoom.price)
+            .reduce((a, b) => a + b, 0)
         }))
       : [];
+
   const lineConfig = {
     data: lineData,
     xField: 'month',
@@ -110,38 +135,15 @@ const Analysis: FC = () => {
           <DatePicker.RangePicker
             picker='month'
             style={{ width: 400, marginBottom: 24 }}
+            format={['MM/YYYY', 'MM/YYYY']}
             ranges={{
-              '1 tháng': [moment().startOf('month'), moment().add(1, 'months')],
-              '3 tháng': [moment().startOf('month'), moment().add(3, 'months')],
-              '6 tháng': [moment().startOf('month'), moment().add(6, 'months')],
-              '1 năm': [moment().startOf('month'), moment().add(12, 'months')]
+              '1 tháng': [moment().startOf('months'), moment().endOf('months')],
+              '3 tháng': [moment().startOf('months'), moment().add(3, 'months').endOf('months')],
+              '6 tháng': [moment().startOf('months'), moment().add(6, 'months').endOf('months')],
+              '1 năm': [moment().startOf('months'), moment().add(12, 'months').endOf('months')]
             }}
-            onChange={async value => {
-              if (value) {
-                const agreements = (await db!.agreement!.toArray()) as ICustomAgreements;
-                setMonthValue(value as any);
-
-                await Promise.all(
-                  agreements.map(async agreement => {
-                    [agreement.room] = (await Promise.all([
-                      db!.room!.get(agreement.roomId)
-                    ])) as ICustomRoom[];
-                    console.log('before');
-                    [agreement.room!.kindOfRoom] = (await Promise.all([
-                      db!.kindOfRoom!.get(agreement.room.kindOfRoomId)
-                    ])) as IKindOfRoom[];
-                    console.log('after');
-                  })
-                );
-
-                setAgreementData(
-                  agreements.filter(
-                    agreement =>
-                      moment(value[0]).isSameOrBefore(agreement.date[0]) &&
-                      moment(value[1]).isSameOrAfter(agreement.date[1])
-                  )
-                );
-              }
+            onChange={value => {
+              setMonthValue([value![0]!.valueOf(), value![1]!.valueOf()]);
             }}
           />
 
